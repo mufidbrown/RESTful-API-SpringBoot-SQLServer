72cdbd6510738d7caec01d6dc38a2a0dd0d9a01c
mufidbrown <ilhammufid67@gmail.com>
2023-10-26 22:44:21 +0700

    create service class

---
+++ src/main/java/proyekmagang/restfullapi/service/ProductService.java
@@ -0,0 +1,23 @@
+package proyekmagang.restfullapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import proyekmagang.restfullapi.entity.Product;
+import proyekmagang.restfullapi.repository.ProductRepository;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+}
---
+++ src/main/java/proyekmagang/restfullapi/service/UserService.java
@@ -0,0 +1,23 @@
+package proyekmagang.restfullapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import proyekmagang.restfullapi.entity.User;
+import proyekmagang.restfullapi.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+}

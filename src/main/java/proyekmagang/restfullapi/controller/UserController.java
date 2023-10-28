51f3710149be64597bfa798e5151ff96e5eaa19c
mufidbrown <ilhammufid67@gmail.com>
2023-10-28 13:13:54 +0700

    update controller & service class

--- src/main/java/proyekmagang/restfullapi/controller/UserController.java
+++ src/main/java/proyekmagang/restfullapi/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import proyekmagang.restfullapi.entity.User;
@@ -15,7 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    
+
     @Autowired
     private UserService userService;
 
@@ -24,5 +25,16 @@ public class UserController {
         List<User> users = userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 
 }
--- src/main/java/proyekmagang/restfullapi/service/UserService.java
+++ src/main/java/proyekmagang/restfullapi/service/UserService.java
@@ -16,4 +16,9 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+
 }

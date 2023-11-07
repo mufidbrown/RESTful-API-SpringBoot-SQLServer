c5b923adf8ecb7c63ebf212094c7faed43992d99
mufidbrown <ilhammufid67@gmail.com>
2023-11-07 11:14:54 +0700

    add controller class

--- src/main/java/proyekmagang/restfullapi/AppRestfullApplication.java
+++ src/main/java/proyekmagang/restfullapi/AppRestfullApplication.java
@@ -1,13 +1,20 @@
 package proyekmagang.restfullapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import proyekmagang.restfullapi.repository.UserRepository;
 
 @SpringBootApplication
 public class AppRestfullApplication {
 
+	@Autowired
+	private UserRepository userRepository;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(AppRestfullApplication.class, args);
 	}
+
 
 }

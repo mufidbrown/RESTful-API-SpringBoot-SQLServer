3291fc1ee256f5d79a76fd00b7fb52bd1f681317
mufidbrown <ilhammufid67@gmail.com>
2023-11-09 08:42:34 +0700

    update exception class

--- src/main/java/proyekmagang/restfullapi/exception/DuplicateUserNameException.java
+++ src/main/java/proyekmagang/restfullapi/exception/DuplicateUserNameException.java
@@ -1,4 +1,18 @@
 package proyekmagang.restfullapi.exception;
 
-public class DuplicateUserNameException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FOUND)
+public class DuplicateUserNameException extends RuntimeException{
+
+    private String message;
+
+    public DuplicateUserNameException( String message) {
+        super(String.format("%s ", message));
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
 }

a3c2fe7aa38ab204d0c97f8f707d541c5f26f91d
mufidbrown <ilhammufid67@gmail.com>
2023-11-09 14:14:32 +0700

    update authentication & authorization

--- src/main/java/proyekmagang/restfullapi/entity/User.java
+++ src/main/java/proyekmagang/restfullapi/entity/User.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 
 @Entity
-@Table(name = "users")
+@Table(name = "customers")
 
 public class User implements UserDetails {
     @Id
--- src/main/java/proyekmagang/restfullapi/repository/UserRepository.java
+++ src/main/java/proyekmagang/restfullapi/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package proyekmagang.restfullapi.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import proyekmagang.restfullapi.entity.User;
@@ -7,6 +8,6 @@ import proyekmagang.restfullapi.entity.User;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }

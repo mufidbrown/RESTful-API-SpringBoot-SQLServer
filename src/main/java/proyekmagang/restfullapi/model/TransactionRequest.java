9e86b83d02ed0b9af596124b932340c52af33e79
mufidbrown <ilhammufid67@gmail.com>
2023-11-21 09:13:48 +0700

    update Service & Model class

---
+++ src/main/java/proyekmagang/restfullapi/model/TransactionRequest.java
@@ -0,0 +1,18 @@
+package proyekmagang.restfullapi.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionRequest {
+
+    private Integer userId;
+
+    private Long productId;
+}

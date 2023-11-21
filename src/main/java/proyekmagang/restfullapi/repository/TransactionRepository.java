51de11f0201340dc28262873145669ab8b454f55
mufidbrown <ilhammufid67@gmail.com>
2023-11-21 09:05:48 +0700

    Add Transaction class

---
+++ src/main/java/proyekmagang/restfullapi/repository/TransactionRepository.java
@@ -0,0 +1,7 @@
+package proyekmagang.restfullapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import proyekmagang.restfullapi.entity.Transaction;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
